3c5b49ed1a738394a6906043f1bac4a3dec9b669
Denis <denistkachenko85@gmail.com>
2019-02-28 18:04:32 +0200

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Lending'
+import Register from './components/register/Register'
+import Login from './components/login/Login'
 import Footer from './components/layout/Footer'
 
 import './App.css'
 
-
 class App extends Component {
   render() {
     return (
-      <div className="AppClasses">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="AppClasses">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+            </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
